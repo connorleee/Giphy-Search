499175188320a1b566cb9131086af593756ce3b6
Connor Lee <45024235+connorleee@users.noreply.github.com>
2019-03-31 17:25:14 -0700

    files and folder structure initiallized

---
+++ assets/javascript/app.js
@@ -0,0 +1,4 @@
+// page will load a few buttons at first from a predefined array. 
+// These buttons will run a function that calls the Giphy API and returns particular values to the screen. 
+// User can input a new button with the submit form and this button will also load from Giphy
+
