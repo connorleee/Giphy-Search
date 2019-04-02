35d9196b7b755842214a082dd99bf1854dec31e5
Connor Lee <45024235+connorleee@users.noreply.github.com>
2019-04-01 22:01:48 -0700

    trying to use a for loop for the ajax in this commit. Going to scratch this and try calling a single api request (no for loop).

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,7 +9,7 @@ var animalList = ["turkey", "dolphin", "tortoise", "dalmation", "goose"];
 function buttonCreator() {
     for (let i = 0; i < animalList.length; i++) {
         var btn = $("<button>")
-            .addClass("btn btn-info mr-2 mb-2 animalButton")
+            .addClass("btn btn-info m-2 animalButton")
             .attr("data-name", animalList[i])
             .text(animalList[i])
         $("#requestButtons").append(btn);
@@ -25,26 +25,49 @@ $("#newItem").click(function () {
         animalList.push(userAnimal);
         $("#requestButtons").empty();
         buttonCreator();
-        
+
         // reset the input form
-        $("#user-animal").val("");     
+        $("#user-animal").val("");
     }
 })
 
-// TODO: function that makes a call to giphy
+// function that makes a call to giphy
+// write the giphy call response to the page (still image and rating)
 function displayGifs() {
     var apiKey = "7hAa0vau8Cuj2wF2cdqwJqZWdjmfU04Z"
-    var q = "dog"
-    // var q = $(this).attr("data-name");
+    var q = $(this).attr("data-name");
     var limit = 10;
     // var rndOffset = Math.floor(Math.random()*1000);
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + q + "&limit=" + limit;
 
     console.log(queryURL)
-    // Need to have an if statement for pg and g only rated
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log("JSON" + response);
+
+        for (let i = 0; i < response.data.length; i++) {
+            // Will need to add logic to add more responses if rating isn't g or pg
+            // only return pg and g rated
+            
+            if(response.data[i].rating === "g" || response.data[i].rating === "pg"){
+                var gifDiv = $("<div class=gif");
+                var stillGifUrl = response.data[i].images.original_still.url;
+                var animatedGifUrl = response.data[i].images.downsized_large.url;
+                var rating = response.data[i].rating;
+
+                pRating = $("<p>").text("Rating: " + rating);
+                console.log(rating);
+                console.log(stillGifUrl);
+                
+            }
+        }
+    })
 }
-displayGifs()
-// TODO: write the giphy call response to the page (still image and rating)
+
+// Adding a click event listener to all elements with a class of "movie-btn"
+$(document).on("click", ".animalButton", displayGifs);
 
 // TODO: click handler on created gifs that will run the gif when clicked
 
